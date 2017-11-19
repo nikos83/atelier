766598d7744f095c64a8ed9f645fbff5c83da264
Funny Bunny <funnybunny@Funnys-Mac.local>
2017-11-19 11:35:14 -0800

    Wysylanie maili sqkickq

--- app/mailers/book_notifier_mailer.rb
+++ app/mailers/book_notifier_mailer.rb
@@ -14,9 +14,12 @@ class BookNotifierMailer < ApplicationMailer
   def book_reserved_return(book)
     @book = book
     @reservation = book.reservations.find_by(status: "RESERVED")
-    @next_borrower = @reservation.user
-     mail(to: @next_borrower.email, subject: "Upływa termin zwrotu książki #{@book.title}") do |format|
+    return "Nie ma nikogo w kolejce" unless @reservation 
+       return "Nie ma oczeującej osoby" unless @next_borrower
+       @next_borrower = @reservation.user
+       mail(to: @next_borrower.email, subject: "Upływa termin zwrotu książki #{@book.title}") do |format|
       format.html
+    
     end
   end
   
